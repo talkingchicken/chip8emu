ba441a08cb2531e6604b2ed6046d18a722c82a3d
talking_chicken <talkingchicken@users.noreply.github.com>
2021-04-27 18:12:05 -0700

    initial csproj commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace chip8_emu
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new ChipEightEmu())
+                game.Run();
+        }
+    }
+}
